4e46ef48a946345f3243de7c129d28d6655d73a5
HARSH <147227325+itsharsh56@users.noreply.github.com>
2026-02-03 23:13:43 +0530

    Time: 0 ms (100.00%) | Memory: 27.7 MB (35.38%) - LeetSync

--- 3952-trionic-array-i/trionic-array-i.cpp
+++ 3952-trionic-array-i/trionic-array-i.cpp
@@ -1,27 +1,25 @@
 class Solution {
 public:
-    bool isInc(vector<int>& a, int l, int r) {
-        for (int i = l; i < r; i++)
-            if (a[i] >= a[i + 1]) return false;
-        return true;
-    }
-
-    bool isDec(vector<int>& a, int l, int r) {
-        for (int i = l; i < r; i++)
-            if (a[i] <= a[i + 1]) return false;
-        return true;
-    }
-
     bool isTrionic(vector<int>& nums) {
         int n = nums.size();
-        for (int p = 1; p <= n - 3; p++) {
-            for (int q = p + 1; q <= n - 2; q++) {
-                if (isInc(nums, 0, p) &&
-                    isDec(nums, p, q) &&
-                    isInc(nums, q, n - 1))
-                    return true;
-            }
-        }
-        return false;
+        if (n < 4) return false;
+
+        int i = 0;
+
+        // check for strictly increasing
+        while (i + 1 < n && nums[i] < nums[i + 1]) i++;
+        if (i == 0) return false;
+
+        // check for strictly decreasing
+        int p = i;
+        while (i + 1 < n && nums[i] > nums[i + 1]) i++;
+        if (i == p) return false;
+
+        // check for strictly increasing
+        int q = i;
+        while (i + 1 < n && nums[i] < nums[i + 1]) i++;
+        if (i == q) return false;
+
+        return i == n - 1;
     }
 };
