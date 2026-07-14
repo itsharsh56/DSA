dea5fedb616848331e17c093b15580287ca860fd
HARSH <147227325+itsharsh56@users.noreply.github.com>
2026-07-14 14:54:57 +0530

    Time: 0 ms (100.00%) | Memory: 7.8 MB (82.37%) - LeetSync

--- 231-power-of-two/power-of-two.cpp
+++ 231-power-of-two/power-of-two.cpp
@@ -2,14 +2,23 @@ class Solution {
 public:
     bool isPowerOfTwo(int n) 
     {
-        if(n<=0){
-        return false;
+        // if(n<=0){
+        // return false;
+        // }
+        // for(long long i = 1 ; i<=n ;i = i*2){
+        //     if (i == n){
+        //      return true;
+        //      }
+        //  }
+        //  return false;
+
+        if(n > 0 && (n &(n - 1)) == 0)
+        {
+            return true ;
         }
-        for(long long i = 1 ; i<=n ;i = i*2){
-            if (i == n){
-             return true;
-             }
-         }
-         return false;
+        else
+        {
+            return false;
+        }
     }
 };
