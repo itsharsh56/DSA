d80cb3149641a3e28855d7daa085cb929f7976cf
HARSH <147227325+itsharsh56@users.noreply.github.com>
2026-07-15 17:36:40 +0530

    Time: 1 ms (44.16%) | Memory: 13.2 MB (99.38%) - LeetSync

--- 137-single-number-ii/single-number-ii.cpp
+++ 137-single-number-ii/single-number-ii.cpp
@@ -18,17 +18,28 @@ public:
 
         // M-2 using count the set bits
 
-        int ans = 0;
-        for (int bitIdx = 0; bitIdx < 32; bitIdx++) {
-            int count = 0;
-            for (int i = 0; i < nums.size(); i++) {
-                if (nums[i] & (1 << bitIdx))
-                count++;
-            }
-            if (count % 3 == 1) {
-                ans = ans | (1 << bitIdx);
-            }
+        // int ans = 0;
+        // for (int bitIdx = 0; bitIdx < 32; bitIdx++) {
+        //     int count = 0;
+        //     for (int i = 0; i < nums.size(); i++) {
+        //         if (nums[i] & (1 << bitIdx))
+        //         count++;
+        //     }
+        //     if (count % 3 == 1) {
+        //         ans = ans | (1 << bitIdx);
+        //     }
+        // }
+        // return ans;
+
+        // M-3 using sorting + iterate with 3
+
+        int n = nums.size();
+        sort(nums.begin(), nums.end());
+
+        for (int i = 1; i < n; i += 3) {
+            if (nums[i] != nums[i - 1])
+                return nums[i - 1];
         }
-        return ans;
+        return nums[n - 1];
     }
 };
