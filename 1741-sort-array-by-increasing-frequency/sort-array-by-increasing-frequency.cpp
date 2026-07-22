0353e942e15c35089b3f22d2a11d77d3281f7d12
HARSH <147227325+itsharsh56@users.noreply.github.com>
2026-07-22 10:43:51 +0530

    Time: 0 ms (100.00%) | Memory: 15.4 MB (22.64%) - LeetSync

---
+++ 1741-sort-array-by-increasing-frequency/sort-array-by-increasing-frequency.cpp
@@ -0,0 +1,44 @@
+class Solution {
+public:
+    vector<int> frequencySort(vector<int>& nums) {
+        int n = nums.size();
+
+        unordered_map<int, int> mp;
+
+        for (int i = 0; i < n; i++) {
+            mp[nums[i]]++;
+        }
+        // M-1 using heap
+        priority_queue<pair<int, int>, vector<pair<int, int>>,
+                       greater<pair<int, int>>>
+            minHeap;
+
+        for (auto i : mp) {
+            minHeap.push({i.second, -i.first});
+        }
+
+        vector<int> ans;
+
+        while (minHeap.size() > 0) {
+            int count = minHeap.top().first;
+            int num = -minHeap.top().second;
+
+            while (count--) {
+                ans.push_back(num);
+            }
+            minHeap.pop();
+        }
+        return ans;
+
+        // M-2 using sort + comparator
+        // sort(nums.begin(), nums.end(), [&](int n1, int n2) {
+        //     if (freq[n1] != freq[n2]) {
+        //         return freq[n1] < freq[n2];
+        //     } else {
+        //         return n2 < n1;
+        //     }
+        // });
+
+        // return nums;
+    }
+};
