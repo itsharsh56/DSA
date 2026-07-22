fe8a97c09ed7f83b9cb1a15ad5ff35327f7b08d9
HARSH <147227325+itsharsh56@users.noreply.github.com>
2026-07-22 11:01:05 +0530

    Time: 7 ms (13.02%) | Memory: 14.9 MB (84.88%) - LeetSync

--- 1741-sort-array-by-increasing-frequency/sort-array-by-increasing-frequency.cpp
+++ 1741-sort-array-by-increasing-frequency/sort-array-by-increasing-frequency.cpp
@@ -9,36 +9,36 @@ public:
             mp[nums[i]]++;
         }
         // M-1 using heap
-        priority_queue<pair<int, int>, vector<pair<int, int>>,
-                       greater<pair<int, int>>>
-            minHeap;
+        // priority_queue<pair<int, int>, vector<pair<int, int>>,
+        //                greater<pair<int, int>>>
+        //     minHeap;
 
-        for (auto i : mp) {
-            minHeap.push({i.second, -i.first});
-        }
+        // for (auto i : mp) {
+        //     minHeap.push({i.second, -i.first});
+        // }
 
-        vector<int> ans;
+        // vector<int> ans;
 
-        while (minHeap.size() > 0) {
-            int count = minHeap.top().first;
-            int num = -minHeap.top().second;
+        // while (minHeap.size() > 0) {
+        //     int count = minHeap.top().first;
+        //     int num = -minHeap.top().second;
 
-            while (count--) {
-                ans.push_back(num);
-            }
-            minHeap.pop();
-        }
-        return ans;
+        //     while (count--) {
+        //         ans.push_back(num);
+        //     }
+        //     minHeap.pop();
+        // }
+        // return ans;
 
         // M-2 using sort + comparator
-        // sort(nums.begin(), nums.end(), [&](int n1, int n2) {
-        //     if (mp[n1] != mp[n2]) {
-        //         return mp[n1] < mp[n2];
-        //     } else {
-        //         return n2 < n1;
-        //     }
-        // });
+        sort(nums.begin(), nums.end(), [&](int n1, int n2) {
+            if (mp[n1] != mp[n2]) {
+                return mp[n1] < mp[n2];
+            } else {
+                return n2 < n1;
+            }
+        });
 
-        // return nums;
+        return nums;
     }
 };
